a7ad1893510cf20d8d80c446023c09c24011cd3b
Andrei Ogurtsov <ogurcovwm@gmail.com>
2024-10-19 20:28:23 +0300

    Solve 02
    Upd 2

--- 02-fuse-helloworld/solution.c
+++ 02-fuse-helloworld/solution.c
@@ -1,4 +1,4 @@
-#ifndef USE_USE_VERSION
+#ifndef FUSE_USE_VERSION
 #define FUSE_USE_VERSION 31
 #endif
 
@@ -16,7 +16,8 @@ static char *create_hello_string() {
     return result;
 }
 
-static int hellofs_getattr(const char *path, struct stat *stbuf) {
+static int hellofs_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
+    (void) fi;
     if (strcmp(path, "/") == 0) {
         stbuf->st_mode = S_IFDIR | 0755;
         stbuf->st_nlink = 2;
@@ -36,14 +37,27 @@ static int hellofs_getattr(const char *path, struct stat *stbuf) {
 
 
 static int hellofs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
-                           struct fuse_file_info *fi) {
-    (void) fi;
+                           struct fuse_file_info *fi, enum fuse_readdir_flags flags) {
     (void) offset;
+    (void) fi;
+    (void) flags;
 
-    if (strcmp(path, "/") != 0) { return -ENOENT; }
-    filler(buf, ".", NULL, 0);
-    filler(buf, "..", NULL, 0);
-    filler(buf, "hello", NULL, 0);
+    struct stat st;
+    memset(&st, 0, sizeof(st));
+
+    if (strcmp(path, "/") != 0) {
+        return -ENOENT;
+    }
+
+    st.st_mode = S_IFDIR | 0755;
+    filler(buf, ".", &st, 0, 0);
+
+    filler(buf, "..", &st, 0, 0);
+
+    st.st_mode = S_IFREG | 0400;
+    st.st_size = strlen(create_hello_string());
+    filler(buf, "hello", &st, 0, 0);
+
     return 0;
 }
 
@@ -54,7 +68,7 @@ static int hellofs_read(const char *path, char *buf, size_t size, off_t offset, 
     const char* content = create_hello_string();
     const int file_size = (int) strlen(content);
     if (offset == file_size) { return 0; }
-    if (offset > file_size) { return EOF; }
+    if (offset > file_size) { return 0; }
     if (size > file_size - (size_t) offset){ size = file_size - offset; }
     memcpy(buf, content + offset, size);
     return (int) size;
