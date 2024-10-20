6475908a93c1f404fe922e179cafd3c7a7ca959b
Andrei Ogurtsov <ogurcovwm@gmail.com>
2024-10-20 15:58:07 +0300

    Solve 13
    Attempt 12

--- 13-realpath/solution.c
+++ 13-realpath/solution.c
@@ -72,7 +72,7 @@ void abspath(const char *path) {
             } else {
                 char *path_to_resolve_tmp = malloc(2 * MAX_PATH_BUFFER_SIZE + 1);
                 if (path_to_resolve_tmp == NULL) { exit(2); }
-                snprintf(path_to_resolve_tmp, 2 * MAX_PATH_BUFFER_SIZE + 1, "%s/%s", symlink_target, path_to_resolve);
+                snprintf(path_to_resolve_tmp, 2 * MAX_PATH_BUFFER_SIZE + 1, "/%s/%s", symlink_target, path_to_resolve);
                 snprintf(path_to_resolve, MAX_PATH_BUFFER_SIZE, "%s", path_to_resolve_tmp);
                 resolved_path[0] = '\0';
                 free(path_to_resolve_tmp);
